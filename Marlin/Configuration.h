743add267729b11ca18edb6f34629ee7998074ca
Joe <privat.lbh@gmail.com>
2023-04-13 01:33:43 +0200

    zprobe offset changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1549,7 +1549,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 30, 0, -2.38 }
+#define NOZZLE_TO_PROBE_OFFSET { 30, 0, -2.55 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
