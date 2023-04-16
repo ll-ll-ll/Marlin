ef9045dd07cc0b53c6b10fec39507ac6143a4dcd
Joe <privat.lbh@gmail.com>
2023-04-16 20:50:53 +0200

    syntax error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1627,7 +1627,7 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   0 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  1.2 // Z Clearance between probe points
+#define Z_CLEARANCE_BETWEEN_PROBES  2 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     0.15 // Z Clearance between multiple probes
 #define Z_AFTER_PROBING           5 // Z position after probing is done
 
